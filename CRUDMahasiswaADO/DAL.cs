6cedc8a2bebf14e35225de250a052a136f9c2d86
Taradwimahendra15 <taradwimahendra15@gmail.com>
2026-06-17 23:13:20 +0700

    Menambahkan fungsi testInject untuk mencegah celah keamanan SQL Injection

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -176,3 +176,16 @@ namespace CRUDMahasiswaADO
             SqlCommand cmdInsert = new SqlCommand(insertQuery, conn);
             cmdInsert.ExecuteNonQuery();
         }
+
+        public void testInject(string nim)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            string query = "Update mahasiswa set nama = 'HACKED' where NIM = '" + nim + "';";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+        }
+
