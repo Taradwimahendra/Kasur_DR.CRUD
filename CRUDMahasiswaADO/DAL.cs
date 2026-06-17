d87b2d388dc409c31bebcf160fb2507268654e77
Taradwimahendra15 <taradwimahendra15@gmail.com>
2026-06-17 22:57:45 +0700

    Menambahkan kelas DAL untuk konfigurasi koneksi database

---
+++ CRUDMahasiswaADO/DAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    internal class DAL
+    {
+
+        static string connectionString = "Data Source=TARA\\TARA;Initial Catalog=DBAkademikADO; User ID=sa;Password=Mahendradwitara";
+
+        public string GetConnectionString()
+        {
+            string connection = $"Data Source={GetLocalIPAddress()};Initial Catalog=DBAkademikADO; User ID=sa;Password=Kadirojo7;";
+            return connectionString;
+        }
