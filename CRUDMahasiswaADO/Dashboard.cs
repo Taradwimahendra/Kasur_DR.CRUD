4c11f5cfb5cab21c56883ae9bbf68e70e007868a
Taradwimahendra15 <taradwimahendra15@gmail.com>
2026-06-19 15:14:41 +0700

    Menambahkan fungsi getDataChartByTahun untuk mengambil data chart berdasarkan parameter tahun

---
+++ CRUDMahasiswaADO/Dashboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Dashboard : Form
+    {
+        static string connectionString = "Data Source=TARA\\TARA;Initial Catalog=DBAkademikADO; User ID=sa;Password=Mahendradwitara";
+
+        DAL dbLogic = new DAL();
+        bool isInitializing = true;
+        DataTable dt;
+        int button = 0;
